8e24c28276ccd9abab739894db18b1aa90e411cd
GuBarone <gugabarone@gmail.com>
2026-08-19 10:51:19 -0300

    feat: estrutura inicial do projeto base

---
+++ app/src/main/java/br/com/aplicacaobase/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.aplicacaobase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
